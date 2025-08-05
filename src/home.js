9cdbb89ce09cb51b8ea8714f138b546de9fdd7ee
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2025-08-05 19:27:20 +0200

    Add comments and change character

--- src/home.js
+++ src/home.js
@@ -1,16 +1,18 @@
 export function renderHome() {
+	// data
 	const textP1 =
 		"Whether you're here to get some work done, meet a friend, or just enjoy a quiet moment, our space is designed to make you feel at ease..";
 	const textP2 =
 		'We keep things simple: quality brews, cozy surroundings. No rush, no noise — just a calm, welcoming place to enjoy a great cup of coffee.';
 
+	// dom
 	const container = document.createElement('div');
 	container.id = 'container__home';
 	container.classList = 'home text-center max-w-lg space-y-5 p-5';
 
 	const h1 = document.createElement('h1');
 	h1.classList = 'text-4xl sm:text-5xl font-bold';
-	h1.innerHTML = `Welcome to <span class="text-primary"> Mosfi's Coffee</span>`;
+	h1.innerHTML = `Welcome to <span class="text-primary"> Mosfi’s Coffee</span>`;
 
 	const p1 = document.createElement('p');
 	p1.textContent = textP1;
