f943798bbff48a8590c2c80515cfd622180af90c
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2025-08-06 19:50:56 +0200

    Import render functions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './styles.css';
 import { renderHome } from './home.js';
 import { renderAbout } from './about.js';
+import { renderMenu } from './menu.js';
 
 const content = document.querySelector('#content');
 
 content.append(renderHome());
+content.append(renderAbout());
+content.append(renderMenu());
