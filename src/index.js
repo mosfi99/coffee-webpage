7a9e4c00ed4ed42d1c2525cad9f0e48becfcc11b
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2025-08-06 21:35:59 +0200

    Add event listeners and content function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ import { renderAbout } from './about.js';
 import { renderMenu } from './menu.js';
 
 const content = document.querySelector('#content');
+const btnMenu = document.querySelector('#menu');
+const btnAbout = document.querySelector('#about');
+const btnHome = document.querySelector('#home');
 
-content.append(renderHome());
-content.append(renderAbout());
-content.append(renderMenu());
+function changeContent(chosenContent) {
+	if (!chosenContent) return;
+	content.textContent = '';
+	content.append(chosenContent);
+}
+
+btnMenu.addEventListener('click', () => {
+	changeContent(renderMenu());
+});
+
+btnAbout.addEventListener('click', () => {
+	changeContent(renderAbout());
+});
+
+btnHome.addEventListener('click', () => {
+	changeContent(renderHome());
+});
+
+changeContent(renderHome());
